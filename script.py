3ac4ac522876b0b4851b71a3f8f3a3803261cfdf
ShihFengLin <noahleft@gmail.com>
2018-11-17 02:16:47 +0800

    run script

---
+++ script.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+
+from testcase_generator.benchGenerator import benchGenerator
+from os import listdir
+
+benchlist = listdir('bench/')
+print(benchlist)
+
+from os import mkdir
+from os.path import exists
+
+def generate_path(names):
+    return '/'.join([str(n) for n in names])
+
+if not exists('testcases'):
+    mkdir('testcases')
+
+for bench in benchlist:
+    dirname = bench[:bench.index('.')]
+    if not exists(generate_path(['testcases',dirname])):
+        mkdir(generate_path(['testcases',dirname]))
+    for idx in range(10):
+        if not exists(generate_path(['testcases',dirname,idx])):
+            mkdir(generate_path(['testcases',dirname,idx]))
+        benchGenerator(generate_path(['bench',bench])).write(generate_path(['testcases',dirname,idx,'case.bench']))
+
